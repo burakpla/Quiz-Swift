ef668333fe6a2e0cf6bad2dbf84d60dee210d19f
Burak Pala <74012242+burakpla@users.noreply.github.com>
2022-01-11 13:55:57 +0300

    finished

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Burak Pala on 11.01.2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String){
+        text = q
+        answer = a
+    }
+}
